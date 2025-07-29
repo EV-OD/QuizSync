7360be30e3ecc2b1c636d0be4ccf70da1f919d8d
Rabin Lamichhane <rabin.lc.web@gmail.com>
2025-07-29 18:40:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="text-r

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <Link href="/" className="flex items-center justify-center gap-2">
         <Image src="/logos/IEEE-CS_LogoTM-orange.png" alt="IEEE Computer Society Logo" width={128} height={128} className="h-10 w-auto dark:[filter:invert(1)_hue-rotate(189deg)_brightness(2)]" />
       </Link>
-      <div className="text-right">
+      <div className="text-right hidden md:block">
         <span className="text-lg md:text-xl font-bold font-headline">Scholars in the Making</span>
         <p className="text-xs text-muted-foreground/80">Organized by IEEE Computer Society Pulchowk Student Branch Chapter</p>
       </div>
