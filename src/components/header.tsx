d46d4277a394172a073bfc7b5b32943abb97b49a
Rabin Lamichhane <rabin.lc.web@gmail.com>
2025-07-29 18:20:22 +0000

    for logo use this filter: invert(1) hue-rotate(191deg);

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-20 px-4 lg:px-6 h-20 flex items-center justify-between bg-background/80 backdrop-blur-sm text-foreground border-b border-border/20">
       <Link href="/" className="flex items-center justify-center gap-2">
-        <Image src="/logos/IEEE-CS_LogoTM-orange.png" alt="IEEE Computer Society Logo" width={128} height={128} className="h-10 w-auto" />
+        <Image src="/logos/IEEE-CS_LogoTM-orange.png" alt="IEEE Computer Society Logo" width={128} height={128} className="h-10 w-auto dark:[filter:invert(1)_hue-rotate(191deg)]" />
       </Link>
       <div className="text-right">
         <span className="text-xl font-bold font-headline">Scholars in the Making</span>
