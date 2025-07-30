d5ba3e354a77a918f393cf33b79b50dc7681372f
Rabin Lamichhane <rabin.lc.web@gmail.com>
2025-07-30 07:10:08 +0000

    commenting feature

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -117,7 +117,7 @@ export default function Home() {
         </section>
 
                 {/* Features Section */}
-          <section id="features" className="w-full py-12 md:py-24 lg:py-32 bg-muted/40 text-foreground">
+          {/* <section id="features" className="w-full py-12 md:py-24 lg:py-32 bg-muted/40 text-foreground">
             <div className="container px-4 md:px-6">
               <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <div className="space-y-2">
@@ -201,10 +201,10 @@ export default function Home() {
                 </GlowCard>
               </div>
             </div>
-          </section>
+          </section> */}
 
                   {/* How it Works Section / Timeline */}
-           {/* <section  id="features" className="w-full py-12 md:py-24 lg:py-32 bg-background text-foreground">
+           <section  id="features" className="w-full py-12 md:py-24 lg:py-32 bg-background text-foreground">
               <div className="container px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
@@ -216,7 +216,7 @@ export default function Home() {
                 </div>
               </div>
               <Timeline data={timelineData} />
-          </section> */}
+          </section>
 
 
 
